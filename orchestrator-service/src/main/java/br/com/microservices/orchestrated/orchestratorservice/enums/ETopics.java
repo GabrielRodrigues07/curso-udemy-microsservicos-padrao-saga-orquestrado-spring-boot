22c7858c94725189a46f0ceffb97817c62b0dd46
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-10-02 21:05:03 -0300

    feat(S3-#2024-10-02) Mapeando e definindo todos os tópicos da arquitetura saga

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/enums/ETopics.java
@@ -0,0 +1,23 @@
+package br.com.microservices.orchestrated.orchestratorservice.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ETopics {
+
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    INVENTORY_SUCCESS("inventory-validation-success"),
+    INVENTORY_FAIL("inventory-validation-fail"),
+    NOTIFY_ENDING("notify-ending");
+
+    private String topic;
+}
