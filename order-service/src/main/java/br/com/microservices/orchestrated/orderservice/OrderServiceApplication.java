1c1f9f3de853c83d9fd7ec9083107157c368ea84
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-10-04 08:59:35 -0300

    feat(S3-#2024-10-04) Configurando a criação automática de tópicos nos serviços

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/OrderServiceApplication.java
@@ -1,9 +1,12 @@
 package br.com.microservices.orchestrated.orderservice;
 
+import br.com.microservices.orchestrated.orderservice.config.kafka.KafkaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KafkaProperties.class)
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
