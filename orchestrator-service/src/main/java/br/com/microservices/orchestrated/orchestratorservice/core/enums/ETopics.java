444f090787f21cc287661f32a08faebdab31bb6f
GabrielRodrigues07 <67024651+GabrielRodrigues07@users.noreply.github.com>
2024-10-12 17:25:49 -0300

    fix(S9-#2024-10-12) Corrigindo nome do topico

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/ETopics.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/ETopics.java
@@ -15,8 +15,8 @@ public enum ETopics {
     PRODUCT_VALIDATION_FAIL("product-validation-fail"),
     PAYMENT_SUCCESS("payment-success"),
     PAYMENT_FAIL("payment-fail"),
-    INVENTORY_SUCCESS("inventory-validation-success"),
-    INVENTORY_FAIL("inventory-validation-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
     NOTIFY_ENDING("notify-ending");
 
     private String topic;
